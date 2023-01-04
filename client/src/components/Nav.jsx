6bb1de52cf559bc7e9b0e303f6501123daae3a13
Cameron Walford <cwalford2012@gmail.com>
2023-01-04 00:31:51 -0800

    Clicking on username brings user to their list.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,6 +7,7 @@ import { useContext } from 'react';
 const Nav = () => {
 
   const { currentUser, logout } = useContext(AuthContext);
+  const usersBooklist = currentUser?.booklistID;
 
   return (
     <div className='nav'>
@@ -16,10 +17,11 @@ const Nav = () => {
             </div>
             <div className="links">
                 <Link className="link" to="/booklist"><h6>LISTS</h6></Link>
-                <Link className="link" to="/?cat=popular"><h6>BOOKS</h6></Link>
+                <Link className="link" to="/book"><h6>BOOKS</h6></Link>
 
                 {/* DISPLAY CURRENT USERNAME */}
-                <span>{currentUser?.username}</span>
+                <Link className ="link" to={`/booklist/${usersBooklist}`}><span>{currentUser?.username}</span></Link>
+                
                 {/* DISPLAY LOG IN IF USER LOGGED OUT, VICE VERSA */}
                 {currentUser ? (<span onClick={logout}>Logout</span>) : (<Link className="link" to="/login">Login</Link>)}
             </div>
